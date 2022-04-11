4c71dc81e028d85422a8bdbf1d51c607140b6f6f
Han <91880235+Mr-Hanbean@users.noreply.github.com>
2022-04-11 10:14:14 +0900

    링크 수정

--- programmers/level1/약수의 합.js
+++ programmers/level1/약수의 합.js
@@ -2,7 +2,7 @@
   문제명: 약수의 합
   문제설명: 
     정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
-  링크: https://programmers.co.kr/learn/courses/30/lessons/42840
+  링크: https://programmers.co.kr/learn/courses/30/lessons/12928
 */
 
 // 문제풀이 1
