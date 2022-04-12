7e3287a179ce25a10bcbbeb3046280aec15db8e5
Han <91880235+Mr-Hanbean@users.noreply.github.com>
2022-04-12 12:06:08 +0900

    링크 수정

--- programmers/level1/실패율.js
+++ programmers/level1/실패율.js
@@ -11,7 +11,7 @@
     전체 스테이지의 개수 N, 게임을 이용하는 사용자가 현재 멈춰있는 스테이지의 번호가 담긴 배열 stages가 매개변수로 주어질 때, 
     실패율이 높은 스테이지부터 내림차순으로 스테이지의 번호가 담겨있는 배열을 return 하도록 solution 함수를 완성하라.
     (...생략)
-  링크: https://programmers.co.kr/learn/courses/30/lessons/77884
+  링크: https://programmers.co.kr/learn/courses/30/lessons/42889
 */
 
 // 문제풀이
